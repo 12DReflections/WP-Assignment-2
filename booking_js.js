8edfedbdedff82c60c97be2f7704b2d87dba8cea
AlberrtZ8411 <einstein96@hotmail.com>
2015-04-27 14:46:20 +1000

    delete the space and functions

--- booking_js.js
+++ booking_js.js
@@ -365,7 +365,7 @@
 
                 });
                 $('#submit_btn').live("click", function () {
-                    $('#film').val(film_name);
+                    $('#film').val(mov_type);
                     $('#day').val($('#day_sel').val());
                     $('#time').val(time);
                     $('#sa').val((parseInt($('#sa_qua').val()) * 1));
@@ -380,25 +380,3 @@
                     $('#book_form').submit()
                 });
             });
-function changeTwoDecimal_f(x)
-{
-var f_x = parseFloat(x);
-if (isNaN(f_x))
-{
-alert('function:changeTwoDecimal->parameter error');
-return false;
-}
-var f_x = Math.round(x*100)/100;
-var s_x = f_x.toString();
-var pos_decimal = s_x.indexOf('.');
-if (pos_decimal < 0)
-{
-pos_decimal = s_x.length;
-s_x += '.';
-}
-while (s_x.length <= pos_decimal + 2)
-{
-s_x += '0';
-}
-return s_x;
-}
